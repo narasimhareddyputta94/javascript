eda61f97430721d8506a720e46b2963c179c782d
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-08-15 15:02:15 -0500

    learning javascript

---
+++ javascript_basics/class_1/new.js
@@ -0,0 +1,55 @@
+console.log("narasimha reddy");
+
+var abc = "hi";
+console.log(abc);
+
+var xyz = 10;
+console.log(xyz);
+
+var pqr = 10.5;
+console.log(pqr);
+
+var mno = true;
+console.log(mno);
+
+var lmn = false;
+console.log(lmn);
+
+let a = 10;
+console.log(a);
+
+let b = 10.5;
+console.log(b);
+
+let c = true;
+console.log(c);
+
+let d = false;
+console.log(d);
+
+let e = "hi";
+console.log(e);
+
+let f = 'hi';
+console.log(f);
+
+const g = 10;
+console.log(g);
+
+const h = 10.5;
+console.log(h);
+
+const i = true;
+console.log(i);
+
+const j = false;
+console.log(j);
+
+const k = "hi";
+console.log(k);
+
+var quantity = 10;
+console.log(quantity++);
+console.log(quantity);
+++quantity;
+console.log(quantity);
